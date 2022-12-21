ebee323c5d2b685ee6d58d90fc0484c5edb9ea7c
FNR <francisnolascoromero@gmail.com>
2022-12-21 14:25:40 +0100

    version 0.0.1 metodo es par y comprobacion

---
+++ Matematicas.java
@@ -0,0 +1,27 @@
+
+
+public class Matematicas {
+    public static void espar(int numero) {
+        if (numero % 2==0){
+            System.out.printf("ES PAR");
+        }else{
+            System.out.printf("ES IMPAR");
+        }
+    }
+    /*public static boolean esDivisiblePorTres(int numero){
+        if (numero % 3==0){
+            System.out.printf("ES DIVISIBLE X3");
+        }else{
+            System.out.printf("NO ES DIVISIBLE X3");
+        }
+        return true;
+    }
+    public static boolean esDivisiblePorCinco(int numero){
+        if (numero % 5==0){
+            System.out.printf("ES DIVISIBLE X5");
+        }else{
+            System.out.printf("NO ES DIVISIBLE X5");
+        }
+        return true;*/
+    }
+
---
+++ TestMatematicas.java
@@ -0,0 +1,15 @@
+
+
+import java.util.Scanner;
+
+public class TestMatematicas {
+
+    public static void main(String[] args) {
+        System.out.printf("INTRODUCE UN NUMERO");
+        Scanner sc = new Scanner(System.in);
+        int numero = sc.nextInt();
+        Matematicas.espar(numero);
+        //Matematicas.esDivisiblePorCinco(numero);
+       // Matematicas.esDivisiblePorTres(numero);
+    }
+}
